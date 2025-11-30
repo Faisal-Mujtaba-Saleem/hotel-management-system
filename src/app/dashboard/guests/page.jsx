6f725f9bb961f731b91b389521beed5373171cd1
Faisal-Mujtaba-Saleem <faisalmujtaba2005@gmail.com>
2025-11-30 16:35:31 +0500

    modified:   src/app/dashboard/guests/page.jsx, Corrected Top Heading

--- src/app/dashboard/guests/page.jsx
+++ src/app/dashboard/guests/page.jsx
@@ -8,7 +8,7 @@ import { FaUserShield } from 'react-icons/fa6';
 export default function page() {
     return (
         <div className='bg-[#F1F5F9] bg-gradient-to-r from-stone-100 to-blue-50 calc-height'>
-            <DashboardSharedHeader Icon={FaUserShield} heading={"Rooms"} />
+            <DashboardSharedHeader Icon={FaUserShield} heading={"Guests"} />
             <GuestsTable />
         </div>
     )
